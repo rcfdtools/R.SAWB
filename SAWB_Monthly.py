489b4cf47b625f7a9c1f7183503330aeb9c24b48
r.cfdtools <r.cfdtools@gmail.com>
2023-05-25 10:31:30 -0500

    General update.

--- SAWB_Monthly.py
+++ SAWB_Monthly.py
@@ -14,13 +14,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Procedure
-path = '.temp/qm/' # Your local .dbf
-join_file = '_Qm.csv' # Joined file name
-pivot_file_qm = '_QmPivot.csv' # Joined average flow file name
-pivot_file_aa = '_AaPivot.csv' # Joined accumulation area file name
-if os.path.isfile(path + join_file):
-    os.remove(path + join_file)
-dbf_files = glob.glob(path + '*.dbf')
+path = '.poi/1/' # Your local path
+join_file = 'Qm.csv' # Joined file name
+pivot_file_qm = 'QmPivot.csv' # Joined average flow file name
+pivot_file_aa = 'AaPivot.csv' # Joined accumulation area file name
+'''
+if os.path.isfile(path + 'qm/' + join_file):
+    os.remove(path + 'qm/' + join_file)
+'''
+dbf_files = glob.glob(path + 'qm/' + '*.dbf')
 df = pd.DataFrame()
 for i in dbf_files:
     print('Processing: %s' %i)
@@ -38,19 +40,19 @@ print(df.dtypes)
 df = df.set_index('date')
 print(df)
 df.to_csv(path+join_file, encoding='utf-8', index=True)
-df.plot(y='SUM', figsize=(9,6), title='Atmospheric monthly average flow serie', ylabel='Qm, m³/s')
+df.plot(y='SUM', figsize=(9,6), title='AWB - Atmospheric monthly average flow serie', ylabel='Qm, m³/s')
 plt.show()
-df.plot(y='Akm2', figsize=(9,6), title='Atmospheric monthly accumulation area serie', ylabel='A, km²')
+df.plot(y='Akm2', figsize=(9,6), title='AWB - Atmospheric monthly accumulation area serie', ylabel='A, km²')
 plt.show()
 pivot_table_qm = df.pivot_table(index='month', columns='year', values='SUM')
 print(pivot_table_qm)
 pivot_table_qm.to_csv(path+pivot_file_qm, encoding='utf-8', index=True)
-pivot_table_qm.plot(y=year_list, figsize=(9,6), title='Atmospheric monthly average flow by year', ylabel='Qm, m³/s')
+pivot_table_qm.plot(y=year_list, figsize=(9,6), title='AWB - Atmospheric monthly average flow by year', ylabel='Qm, m³/s')
 plt.show()
 pivot_table_aa = df.pivot_table(index='month', columns='year', values='Akm2')
 print(pivot_table_aa)
 pivot_table_aa.to_csv(path+pivot_file_aa, encoding='utf-8', index=True)
-pivot_table_aa.plot(y=year_list, figsize=(9,6), title='Atmospheric monthly accumulation area by year', ylabel='A, km²')
+pivot_table_aa.plot(y=year_list, figsize=(9,6), title='AWB - Atmospheric monthly accumulation area by year', ylabel='A, km²')
 #plt.xticks(range(len(pivot_table_aa)),labels=range(0, len(pivot_table_aa)))
 plt.show()
 
