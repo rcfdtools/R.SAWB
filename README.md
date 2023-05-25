d8a5ee9a9386326a1f31ce3a36f6ddd3f0823d23
r.cfdtools <r.cfdtools@gmail.com>
2023-05-25 14:28:03 -0500

    General update.

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # R.SAWB
-Surface Water Balance (SWB) &amp; Atmospheric Water Balance (AWB)
+Surface Water Balance (SWB) &amp; Atmospheric Water Balance (AWB)  
+https://cds.climate.copernicus.eu/
 
+## Search1: ERA5-Land monthly averaged data from 1950 to present
+	
+* Resolution: 0.1dd x 0.1dd
+* Submission date: 2023-05-06 10:13:33
+* Size: 1.0 GB+
+* Product type: Monthly averaged reanalysis
+* Variables: 10m u-component of wind, 10m v-component of wind, 2m temperature, Total evaporation, Total precipitation
+* Year: 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2022
+* Month: January, February, March, April, May, June, July, August, September, October, November, December
+* Time: 00:00
+* Sub-region extraction: North 16°, West -96°, South -58°, East -25°
+* Format:GRIB & NetCDF
+
+
+## Search 2: ERA5 hourly data on single levels from 1940 to present (land & seas)
+
+* Resolution: 0.25dd x 0.25dd
+* Variables: 10m u-component of wind, 10m v-component of wind, 2m temperature, Evaporation, Total precipitation
+
+
+## Search 3: ERA5 monthly averaged data on single levels from 1940 to present (land & sea)
+	
+* Resolution: 0.25dd x 0.25dd
+* Variables: 10m u-component of wind, 10m v-component of wind, 2m temperature, Evaporation, Total precipitation
 
 
 ## References
 
 * https://www.caee.utexas.edu/prof/maidment/gishyd97/atmos/atmos.htm
 * [Conjoint Analysis of Surface and Atmospheric Water Balances in the Andes-Amazon System](https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2017WR021338)
+* https://edo.jrc.ec.europa.eu/documents/factsheets/factsheet_spi_ado.pdf
+* [How to calculate PET, SPI, SPEI and Palmer drought indices in Python?](https://www.youtube.com/watch?v=WMF45KQiQAM)
+* [Standardized Precipitation Index (SPI) | Drought & Flood Monitor](https://www.youtube.com/watch?v=zYT5VpQWJAQ)
--- SAWB_Monthly.py
+++ SAWB_Monthly.py
@@ -13,22 +13,20 @@ from dbfread import DBF
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Procedure
+# Variables
 path = '.poi/1/' # Your local path
 join_file = 'Qm.csv' # Joined file name
 pivot_file_qm = 'QmPivot.csv' # Joined average flow file name
 pivot_file_aa = 'AaPivot.csv' # Joined accumulation area file name
-'''
-if os.path.isfile(path + 'qm/' + join_file):
-    os.remove(path + 'qm/' + join_file)
-'''
 dbf_files = glob.glob(path + 'qm/' + '*.dbf')
 df = pd.DataFrame()
+show_plot = False  # Verbose plot
+
+# Procedure
 for i in dbf_files:
     print('Processing: %s' %i)
     dbf = DBF(i)
     dataResult = pd.DataFrame(iter(dbf))
-    #print(dataResult)
     df = pd.concat([df, dataResult], ignore_index=False)
 min_year = df['year'].min()
 max_year = df['year'].max()
@@ -40,21 +38,26 @@ print(df.dtypes)
 df = df.set_index('date')
 print(df)
 df.to_csv(path+join_file, encoding='utf-8', index=True)
-df.plot(y='SUM', figsize=(9,6), title='AWB - Atmospheric monthly average flow serie', ylabel='Qm, m³/s')
-plt.show()
-df.plot(y='Akm2', figsize=(9,6), title='AWB - Atmospheric monthly accumulation area serie', ylabel='A, km²')
-plt.show()
+df.plot(y='SUM', figsize=(10,6), title='AWB - Atmospheric monthly average flow serie', ylabel='Qm, m³/s')
+plt.savefig(path + 'Graph/Plot_QmSerie.png')
+if show_plot: plt.show()
+df.plot(y='Akm2', figsize=(10,6), title='AWB - Atmospheric monthly accumulation area serie', ylabel='A, km²')
+plt.savefig(path + 'Graph/Plot_AaSerie.png')
+if show_plot: plt.show()
 pivot_table_qm = df.pivot_table(index='month', columns='year', values='SUM')
 print(pivot_table_qm)
 pivot_table_qm.to_csv(path+pivot_file_qm, encoding='utf-8', index=True)
-pivot_table_qm.plot(y=year_list, figsize=(9,6), title='AWB - Atmospheric monthly average flow by year', ylabel='Qm, m³/s')
-plt.show()
+pivot_table_qm.plot(y=year_list, figsize=(10,6), title='AWB - Atmospheric monthly average flow by year', ylabel='Qm, m³/s')
+plt.savefig(path + 'Graph/Plot_QmMonthly.png')
+if show_plot: plt.show()
 pivot_table_aa = df.pivot_table(index='month', columns='year', values='Akm2')
 print(pivot_table_aa)
 pivot_table_aa.to_csv(path+pivot_file_aa, encoding='utf-8', index=True)
-pivot_table_aa.plot(y=year_list, figsize=(9,6), title='AWB - Atmospheric monthly accumulation area by year', ylabel='A, km²')
+pivot_table_aa.plot(y=year_list, figsize=(10,6), title='AWB - Atmospheric monthly accumulation area by year', ylabel='A, km²')
 #plt.xticks(range(len(pivot_table_aa)),labels=range(0, len(pivot_table_aa)))
-plt.show()
+plt.savefig(path + 'Graph/Plot_AaMonthly.png')
+if show_plot: plt.show()
+plt.close('all')
 
 
 # References
